171ba6363d49dde9a59ef6d8a7b4aee4d0ab0bd4
Maksim I. Fedorchenko <fmaksim74@gmail.com>
2019-08-31 15:11:39 +0500

    Foreign key added

--- DemoDB1/dbo/Tables/tblPersons.sql
+++ DemoDB1/dbo/Tables/tblPersons.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[Table2]
+﻿CREATE TABLE [dbo].[tblPersons]
 (
 	[ID] INT NOT NULL  IDENTITY(1, 1) CONSTRAINT PK_tblPersons_ID PRIMARY KEY, 
 	[CreateDate] DATETIME NOT NULL CONSTRAINT DF_tblPersons_CreateDate DEFAULT CURRENT_TIMESTAMP,
@@ -7,7 +7,8 @@
 	[MiddleName] NVARCHAR(128) NULL,
 	[LastName] NVARCHAR(128) NULL,
 	[Gender] CHAR(1) CONSTRAINT CK_tblPersons_Gender CHECK (Gender IN ('M','F'))
-					 CONSTRAINT DF_tglPersons_Gender DEFAULT 'M'
+					 CONSTRAINT DF_tglPersons_Gender DEFAULT 'M', 
+    CONSTRAINT [FK_tblPersons_tblUsers] FOREIGN KEY ([LoginID]) REFERENCES [tblUsers]([ID])
 )
 
 GO
@@ -16,6 +17,6 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
-    @level1name = N'Table2',
+    @level1name = N'tblPersons',
     @level2type = N'COLUMN',
     @level2name = N'ID'
