cefb118c4c96687ee5e78ba775eb374d4c8f786b
Maksim I. Fedorchenko <fmaksim74@gmail.com>
2019-08-31 14:55:41 +0500

    Добавьте файлы проекта.

---
+++ DemoDB1/dbo/Tables/tblPersons.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+	[ID] INT NOT NULL  IDENTITY(1, 1) CONSTRAINT PK_tblPersons_ID PRIMARY KEY, 
+	[CreateDate] DATETIME NOT NULL CONSTRAINT DF_tblPersons_CreateDate DEFAULT CURRENT_TIMESTAMP,
+    [LoginID] INT NOT NULL CONSTRAINT DF_tblPersons_LoginID DEFAULT 0,
+	[FirstName] NVARCHAR(128) NULL,
+	[MiddleName] NVARCHAR(128) NULL,
+	[LastName] NVARCHAR(128) NULL,
+	[Gender] CHAR(1) CONSTRAINT CK_tblPersons_Gender CHECK (Gender IN ('M','F'))
+					 CONSTRAINT DF_tglPersons_Gender DEFAULT 'M'
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Идентификатор персоны',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Table2',
+    @level2type = N'COLUMN',
+    @level2name = N'ID'
---
+++ DemoDB1/dbo/Tables/tblUsers.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[ID] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_tblUsers_ID PRIMARY KEY, 
+    [Login] NVARCHAR(64) NOT NULL CONSTRAINT UC_tblUsers_Login UNIQUE, 
+    [CreataDate] DATETIME NOT NULL CONSTRAINT DF_tblUsers_CreateDate DEFAULT CURRENT_TIMESTAMP, 
+	[State] TINYINT NOT NULL CONSTRAINT DF_tblUsers_State DEFAULT 0, 
+    [ApplicationConfig] NTEXT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Внутренний идентификатор пользователя',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Table1',
+    @level2type = N'COLUMN',
+    @level2name = N'ID'
