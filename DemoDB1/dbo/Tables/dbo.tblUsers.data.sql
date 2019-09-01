f2075b7577ff4de94e2845f01d6b4069527fa012
Maksim I. Fedorchenko <fmaksim74@gmail.com>
2019-09-01 22:14:33 +0500

    Changes

---
+++ DemoDB1/dbo/Tables/dbo.tblUsers.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[tblUsers] ON
+INSERT INTO [dbo].[tblUsers] ([ID], [Login], [CreataDate], [State], [ApplicationConfig], [Note]) VALUES (1, N'user1', N'2019-08-31 13:13:02', 0, NULL, NULL)
+INSERT INTO [dbo].[tblUsers] ([ID], [Login], [CreataDate], [State], [ApplicationConfig], [Note]) VALUES (2, N'user2', N'2019-08-31 13:13:07', 0, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[tblUsers] OFF
